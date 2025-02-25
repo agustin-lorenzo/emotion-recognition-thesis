27c07002507a63f88dcf30145fb3c91ad586861f
agustin-lorenzo <lorenzo.agustin.al@gmail.com>
2025-02-25 15:04:25 -0500

    Added script for saving train and test splits to disk, accounted for change in cross_subject.py, began cross_subject training

--- cross_subject.py
+++ cross_subject.py
@@ -1,9 +1,6 @@
-import fcwt
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 import torch
 import torch.optim as optim
@@ -11,17 +8,18 @@ import torch.nn as nn
 from vit_pytorch.vit_3d import ViT
 import random
 import csv
-import os
-import math
 import copy
+import gc
+import os
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # initialize constants
 SNR = 5 # signal-to-noise ratio
 REP_FACTOR = 1 # how many augmented samples are created out of one original sample
-NUM_EPOCHS = 20 # number of epochs for training
+NUM_EPOCHS = 50 # number of epochs for training
 
 csv_file = f'cross_subject_model_metrics_{NUM_EPOCHS}epochs.csv'
 header = ["subject", "valence_accuracy", "arousal_accuracy", "overall_accuracy"]
@@ -32,12 +30,13 @@ if not os.path.exists(csv_file):
         writer.writerow(header)
 
 # helper method for adding gaussian noise to each frame in a sample, includes signal-to-noise parameter
-def add_gaussian_noise(signal, snr_db=5):
-    signal_power = np.mean(signal ** 2)
+# altered to handle pytorch tensors rather than numpy array
+def add_gaussian_noise_torch(signal_tensor, snr_db=5):
+    signal_power = (signal_tensor ** 2).mean()
     noise_power = signal_power / (10 ** (snr_db / 10))
-    noise = np.random.normal(0, math.sqrt(noise_power), signal.shape)
-    noisy_signal = signal + noise
-    return noisy_signal
+    noise_std = torch.sqrt(noise_power)
+    noise = torch.randn_like(signal_tensor) * noise_std
+    return signal_tensor + noise
 
 # dataset object for CWT data
 class CWTDataset(Dataset):
@@ -65,9 +64,12 @@ class CWTDataset(Dataset):
                     for frame in x:
                         noisy_frame = copy.deepcopy(frame)
                         for channel in range(32):
-                            noisy_frame[:, channel] = add_gaussian_noise(noisy_frame[:, channel], SNR)
+                            noisy_frame[:, :, channel] = add_gaussian_noise_torch(noisy_frame[:, :, channel], SNR)
                         augmented_sample.append(noisy_frame)
-                    return augmented_sample, label
+
+                    augmented_tensor = torch.stack(augmented_sample, dim=0)
+                    return augmented_tensor, label
+                
             return x, label
 
         def train(self):
@@ -77,88 +79,6 @@ class CWTDataset(Dataset):
         def eval(self):
             self.training = False
             return self
-
-six_sec_samples = [] # all 6-second samples from all subjects in the dataset
-sample_classes = []  # all target classes for each of the 6-second samples
-
-for subject in range(32):
-    file_path = f"data_preprocessed_python/s{subject + 1:02}.dat"
-    x = pickle.load(open(file_path, 'rb'), encoding='latin1')
-    data = x['data']
-    labels = x['labels']
-    relevant_channels = data[:, :32, :]
-    relevant_labels = labels[:, :2]
-    classes = []
-
-    for trial in range(40):
-        # 4 valence-arousal classes
-        valence, arousal = relevant_labels[trial][0], relevant_labels[trial][1]
-        if valence < 4.5 and arousal < 4.5: # LVLA
-            cls = 0
-        elif valence < 4.5:                 # LVHA
-            cls = 1
-        elif arousal < 4.5:                 # HVLA
-            cls = 2
-        else:                               # HVHA
-            cls = 3
-        classes.append(cls)
-
-    # initialize constant variables
-    # paramaters for calculating cwt, not to be changed
-    fs = 128 
-    f0 = 4 # lowest frequency
-    f1 = 45 # highest frequency
-    fn = 32 # number of frequencies, match channel number for square frame
-
-    print(f"Creating frames from participant {subject + 1}'s EEG data...")
-    for trial in range(40):
-        total_cwt = np.zeros((1024, 8064))
-
-        for channel in range(32):    
-            signal = relevant_channels[trial][channel]
-            _, current_cwt = fcwt.cwt(signal, fs, f0, f1, fn)
-            start = channel * fn
-            end = (channel + 1) * fn
-            total_cwt[start:end, :] = abs(current_cwt)
-        
-        # convert 2D time sample x channel-frequency format (x, y) to 3D channel x frequency x time format (x, y, z)
-        # each 'frame' in all_frames is a 2D image showing each channel's CWT value for that time sample
-        all_frames = []
-        for sample in range(8064):
-            frame = np.zeros((32, 32))
-            for channel in range(32):
-                start = channel * fn
-                end = (channel + 1) * fn
-                frame[:, channel] = total_cwt[start:end, sample]
-            # normalize frame
-            norm_frame = (frame - frame.min()) / (frame.max() - frame.min())
-            # scale to 0-255
-            scaled_frame = (norm_frame * 255).astype(np.uint8)
-            # stack for pseudo RGB
-            frame_rgb = np.stack((scaled_frame,) * 3, axis=0)
-            frame_rgb = np.clip(frame_rgb, 0, 255).astype(np.uint8)
-            all_frames.append(frame_rgb)
-            #all_frames.append(frame) # append 1-channel frame without rgb
-        
-        # split trial into 6-second samples as suggested in Arjun et al. 2021
-        window_size = 6 * 128
-        for i in range(0, 8064, window_size):
-            if i + window_size > 8064:
-                break
-            original_sample = all_frames[i:i+window_size]
-            six_sec_samples.append(original_sample)
-            sample_classes.append(classes[trial])
-
-print("All frames and copies initialized.")
-print("\nCreating dataset objects...")    
-train_data, test_data, train_labels, test_labels = train_test_split(
-    six_sec_samples, sample_classes, test_size=0.2, random_state=23, stratify=sample_classes
-)
-train_loader = DataLoader(CWTDataset(train_data, train_labels).train(), batch_size=4, shuffle=True)
-test_loader = DataLoader(CWTDataset(test_data, test_labels).eval(), batch_size=4)
-print("Dataset objects initialized.")
-print(f"\nTrain dataset shape:\n\t{np.shape(train_data)}")
-print(f"Test dataset shape:\n\t{np.shape(test_data)}")
 
 vit = ViT( # vision transformer parameters as suggested by Awan et al. 2024
     image_size=32,
@@ -175,6 +95,16 @@ vit = ViT( # vision transformer parameters as suggested by Awan et al. 2024
     emb_dropout=0.1,
     pool='mean'
 ).to(device)
+
+
+# model training
+print("Loading training data...")
+train_file = np.load("test_data.npz", allow_pickle=True) # read train data from disk
+train_data = train_file["samples"]
+train_labels = train_file["labels"]
+train_loader = DataLoader(CWTDataset(train_data, train_labels).train(), batch_size=4, shuffle=True)
+print("Training data initialized.")
+print(f"\nTrain dataset shape:\n\t{np.shape(train_data)}")
 
 loss_fn = nn.CrossEntropyLoss() # cross entropy is appropriate for classification
 optimizer = optim.AdamW(vit.parameters(), lr=3e-4, weight_decay=0.01)
@@ -198,12 +128,24 @@ for epoch in range(NUM_EPOCHS):
         optimizer.step()
     scheduler.step()
 print("\nFinished Training.")
+del train_loader, train_data, train_labels, train_file # free up space
+gc.collect()
 
+
+# model testing
 vit.eval()
 valence_correct = 0 # valence-specific accuracy
 arousal_correct = 0 # arousal-specific accuracy
 overall_correct = 0 # overall accuracy in classifying accross 4 possible cases
 total = 0
+
+print("Loading testing data...")
+test_file = np.load("test_data.npz", allow_pickle=True) # read train data from disk
+test_data = test_file["samples"]
+test_labels = test_file["labels"]
+test_loader = DataLoader(CWTDataset(test_data, test_labels).eval(), batch_size=4)
+print("Testing data initalized.")
+print(f"Test dataset shape:\n\t{np.shape(test_data)}")
 
 # lists for confusion matrix
 pred = []
@@ -236,10 +178,10 @@ print("---------------------------------------")
 # save metrics to .csv
 with open(csv_file, mode='a', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow([subject + 1, valence_accuracy, arousal_accuracy, overall_accuracy])
+    writer.writerow(["all", valence_accuracy, arousal_accuracy, overall_accuracy])
     file.flush()  # flush ensures the row is written immediately
 
-torch.save(vit.state_dict(), f"models/cross_subject_model.pth")
+torch.save(vit.state_dict(), f"models/cross_subject_model_{NUM_EPOCHS}epochs.pth")
 
 # creating confusion matrix
 cm = confusion_matrix(y_test, pred)
---
+++ presave_cwt.py
@@ -0,0 +1,104 @@
+import fcwt
+import numpy as np
+import pickle
+import math
+from sklearn.model_selection import train_test_split
+
+# initialize constants
+SNR = 5 # signal-to-noise ratio
+REP_FACTOR = 1 # how many augmented samples are created out of one original sample
+NUM_EPOCHS = 20 # number of epochs for training
+
+# helper method for adding gaussian noise to each frame in a sample, includes signal-to-noise parameter
+def add_gaussian_noise(signal, snr_db=5):
+    signal_power = np.mean(signal ** 2)
+    noise_power = signal_power / (10 ** (snr_db / 10))
+    noise = np.random.normal(0, math.sqrt(noise_power), signal.shape)
+    noisy_signal = signal + noise
+    return noisy_signal
+
+six_sec_samples = [] # all 6-second samples from all subjects in the dataset
+sample_classes = []  # all target classes for each of the 6-second samples
+
+for subject in range(32):
+    file_path = f"data_preprocessed_python/s{subject + 1:02}.dat"
+    x = pickle.load(open(file_path, 'rb'), encoding='latin1')
+    data = x['data']
+    labels = x['labels']
+    relevant_channels = data[:, :32, :]
+    relevant_labels = labels[:, :2]
+    classes = []
+
+    for trial in range(40):
+        # 4 valence-arousal classes
+        valence, arousal = relevant_labels[trial][0], relevant_labels[trial][1]
+        if valence < 4.5 and arousal < 4.5: # LVLA
+            cls = 0
+        elif valence < 4.5:                 # LVHA
+            cls = 1
+        elif arousal < 4.5:                 # HVLA
+            cls = 2
+        else:                               # HVHA
+            cls = 3
+        classes.append(cls)
+
+    # initialize constant variables
+    # paramaters for calculating cwt, not to be changed
+    fs = 128 
+    f0 = 4 # lowest frequency
+    f1 = 45 # highest frequency
+    fn = 32 # number of frequencies, match channel number for square frame
+
+    print(f"Creating frames from participant {subject + 1}'s EEG data...")
+    for trial in range(40):
+        total_cwt = np.zeros((1024, 8064))
+
+        for channel in range(32):    
+            signal = relevant_channels[trial][channel]
+            _, current_cwt = fcwt.cwt(signal, fs, f0, f1, fn)
+            start = channel * fn
+            end = (channel + 1) * fn
+            total_cwt[start:end, :] = abs(current_cwt)
+        
+        # convert 2D time sample x channel-frequency format (x, y) to 3D channel x frequency x time format (x, y, z)
+        # each 'frame' in all_frames is a 2D image showing each channel's CWT value for that time sample
+        all_frames = []
+        for sample in range(8064):
+            frame = np.zeros((32, 32))
+            for channel in range(32):
+                start = channel * fn
+                end = (channel + 1) * fn
+                frame[:, channel] = total_cwt[start:end, sample]
+            # normalize frame
+            norm_frame = (frame - frame.min()) / (frame.max() - frame.min())
+            # scale to 0-255 (although this is normalized in dataset object later, 
+            #                 it's still best to keep the input in a "video format")
+            scaled_frame = (norm_frame * 255).astype(np.uint8)
+            # stack for pseudo RGB
+            frame_rgb = np.stack((scaled_frame,) * 3, axis=0)
+            frame_rgb = np.clip(frame_rgb, 0, 255).astype(np.uint8)
+            all_frames.append(frame_rgb)
+            #all_frames.append(frame) # append 1-channel frame without rgb
+        
+        # split trial into 6-second samples as suggested in Arjun et al. 2021
+        window_size = 6 * 128
+        for i in range(0, 8064, window_size):
+            if i + window_size > 8064:
+                break
+            original_sample = all_frames[i:i+window_size]
+            six_sec_samples.append(original_sample)
+            sample_classes.append(classes[trial])
+
+        # TODO: come back and re-add augmented frames with gaussian noise if 2560 samples aren't enough for cross-sub model
+
+# save train/test splits to disk
+print("All frames initialized.")
+print("\nCreating train/test splits...")    
+train_data, test_data, train_labels, test_labels = train_test_split(
+    six_sec_samples, sample_classes, test_size=0.2, random_state=23, stratify=sample_classes
+)
+print("Saving to disk...")
+np.savez_compressed("train_data.npz", samples=train_data, labels=train_labels)
+np.savez_compressed("test_data.npz", samples=test_data, labels=test_labels)
+del six_sec_samples, sample_classes, train_data, test_data, train_labels, test_labels
+print("\nDone.")
